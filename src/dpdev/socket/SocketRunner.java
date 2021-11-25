bf363a6232056feb41a44b1105f5e1f08dde3710
Dzianis Pikus <dzianis_pikus@epam.com>
2021-11-25 17:32:32 +0300

    Initial commit

---
+++ src/dpdev/socket/SocketRunner.java
@@ -0,0 +1,21 @@
+package dpdev.socket;
+
+import java.io.*;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class SocketRunner {
+    public static void main(String[] args) throws IOException {
+        var inetAddress = Inet4Address.getByName("google.com");
+        try (var socket = new Socket(inetAddress, 80);
+             var outputStream = new DataOutputStream(socket.getOutputStream());
+             var inputStream = new DataInputStream(socket.getInputStream())) {
+            outputStream.writeUTF("Hello world!");
+            var response = inputStream.readAllBytes();
+            System.out.println(response);
+
+        }
+
+    }
+}
