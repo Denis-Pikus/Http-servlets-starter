e029678daf3add568820bcba051a4c0a834a9f3e
Dzianis_Pikus <denis.pikus@gmail.com>
2021-11-26 12:22:54 +0300

    Add HttpClient from Java 11

---
+++ src/dpdev/socket/UrlExample.java
@@ -0,0 +1,14 @@
+package dpdev.socket;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class UrlExample {
+    public static void main(String[] args) throws IOException {
+        var url = new URL("https://www.google.com");
+        var urlConnection = url.openConnection();
+
+        System.out.println(urlConnection);
+    }
+}
